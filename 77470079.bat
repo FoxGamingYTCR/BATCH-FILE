0935cf0677c5e64f9290786234ec28f92701918c
FOX GAMINGYT <105371123+FoxGamingYTCR@users.noreply.github.com>
2022-05-11 13:00:11 -0400

    funny fake virus prank (flashing lights warning)
    it is just a prank and it has a lot of flashing/changing colors so be aware of that... it is fun to use on friends though or make your parents/friends think you've been hacked!! XD

---
+++ 77470079.bat
@@ -0,0 +1,343 @@
+@echo off
+if exist nuclear.bat goto :next
+echo @echo off >> nuclear.bat
+echo color 2 >> nuclear.bat
+echo echo %random% >> nuclear.bat
+echo color 4 >> nuclear.bat
+echo color 20 >> nuclear.bat
+echo %random% >> nuclear.bat
+echo color 2 >> nuclear.bat
+echo color 4 >> nuclear.bat
+echo echo %random% >> nuclear.bat
+echo color 40 >> nuclear.bat
+echo color 20 >> nuclear.bat
+echo %random% >> nuclear.bat
+echo color 2 >> nuclear.bat
+echo color 4 >> nuclear.bat
+echo echo %random% >> nuclear.bat
+echo color 4 >> nuclear.bat
+echo color 20 >> nuclear.bat
+echo %random% >> nuclear.bat
+echo color 2 >> nuclear.bat
+echo color 4 >> nuclear.bat
+echo echo %random% >> nuclear.bat
+echo color 40 >> nuclear.bat
+echo color 20 >> nuclear.bat
+echo %random% >> nuclear.bat
+echo color 4 >> nuclear.bat
+echo exit >> nuclear.bat
+:next
+color 40
+color 20
+color 4
+color 2
+timeout /t 1 >nul
+color 4
+echo virus?
+echo ...
+timeout /t 2 >nul
+color 20
+echo soRR Y b UUt tt tthere iiSSS aNNNN ERRORORoORRRRR ######?????!!!
+timeout /t 1 >nul
+color 2
+ERROR
+color 40
+virus
+color 2
+timeout /t 1 >nul
+color 4
+AAAA??
+timeout /t 2 >nul
+color 20
+EEERRRRRR???
+color 40
+color 2
+timeout /t 1 >nul
+color 40
+0010499
+color 2
+timeout /t 1 >nul
+HAx
+949??
+echo XXXDDD
+timeout /t 2 >nul
+color 4
+VOID VOID AAE??RRF
+ekakJEJAkea
+echo hacked
+timeout /t 1 >nul
+color 20
+EAIKEkaK
+timeout /t 1 >nul
+AKEKAkej
+color 4
+aEOE???
+color 40
+timeout /t 2 >nul
+color 2
+INFECTION
+timeout /t 1 >nul
+color 4
+echo PREDAAAaTOOORRR??? IS TAKINGGGGG OVERRRRR?????!!!
+timeout /t 2 >nul
+cls
+echo . . .
+ping localhost >nul
+color 4
+cls
+echo we are coming for you %username%...
+echo your device (%computername%) will be our new host...
+echo we will spread and destroy everything...
+ping localhost /n 5 >nul
+cls
+echo 7
+timeout /t 2 >nul
+echo 7
+timeout /t 2 >nul
+echo 4
+timeout /t 2 >nul
+echo 7
+timeout /t 2 >nul
+echo 0
+timeout /t 1 >nul
+echo 0
+timeout /t 1 >nul
+echo 7
+timeout /t 1 >nul
+echo 9
+timeout /t 1 >nul
+cls
+echo 7
+timeout /t 1 >nul
+echo 7
+timeout /t 1 >nul
+echo 4
+timeout /t 1 >nul
+echo 7
+timeout /t 1 >nul
+echo 0
+timeout /t 1 >nul
+echo 0
+timeout /t 0 >nul
+echo 7
+timeout /t 0 >nul
+echo 9
+timeout /t 0 >nul
+cls
+echo 7
+color 2
+timeout /t 0 >nul
+echo 7
+timeout /t 0 >nul
+echo 4
+timeout /t 0 >nul
+echo 7
+color 40
+color 2
+timeout /t 0 >nul
+echo 0
+timeout /t 0 >nul
+echo 0
+timeout /t 0 >nul
+echo 7
+color 4
+timeout /t 0 >nul
+echo 9
+timeout /t 0 >nul
+%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+color 40
+start nuclear.bat
+%random%%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+echo %random%%random%
+timeout /t 0 >nul
+color 4
+color 20
+echo %random%%random%%random%%random%
+timeout /t 0 >nul
+%random%%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+msg * DIE!!
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+color 2
+start nuclear.bat
+%random%%random%%random%
+timeout /t 0 >nul
+echo %random%
+timeout /t 0 >nul
+%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+color 20
+color 40
+%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+start nuclear.bat
+msg * hax!!
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+color 4
+color 40
+%random%%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+start nuclear.bat
+echo %random%%random%
+timeout /t 0 >nul
+color 2
+start nuclear.bat
+echo %random%%random%%random%%random%
+timeout /t 0 >nul
+msg * XD
+%random%%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+start nuclear.bat
+%random%%random%%random%
+timeout /t 0 >nul
+color 40
+echo %random%
+timeout /t 0 >nul
+%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+start nuclear.bat
+color 4
+msg * DIE!!
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+%random%%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+start nuclear.bat
+echo %random%%random%
+timeout /t 0 >nul
+color 40
+color 2
+msg * XD
+echo %random%%random%%random%%random%
+timeout /t 0 >nul
+%random%%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+start nuclear.bat
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+timeout /t 0 >nul
+color 4
+%random%%random%%random%
+timeout /t 0 >nul
+start nuclear.bat
+echo %random%
+msg * DIE!!
+timeout /t 0 >nul
+HACKED HACKED HACKED HACKED
+start nuclear.bat
+timeout /t 0 >nul
+color 20
+color 4
+echo ATTACKING SERVER
+timeout /t 0 >nul
+msg * hax
+echo ATTACKING SERVER
+start nuclear.bat
+timeout /t 0 >nul
+echo ATTACKING SERVER
+timeout /t 0 >nul
+start nuclear.bat
+color 2
+msg * hax
+color 40
+echo ATTACKING SERVER
+timeout /t 0 >nul
+echo ATTACKING SERVER
+start nuclear.bat
+timeout /t 0 >nul
+echo ATTACKING SERVER
+timeout /t 0 >nul
+msg * XD
+echo ATTACKING SERVER
+timeout /t 0 >nul
+echo ATTACKING SERVER
+timeout /t 0 >nul
+start nuclear.bat
+color 4
+color 20
+color 40
+echo ATTACKING SERVER
+start nuclear.bat
+timeout /t 0 >nul
+msg * DIE!!
+echo ATTACKING SERVER
+timeout /t 0 >nul
+start nuclear.bat
+echo ATTACKING SERVER
+timeout /t 0 >nul
+echo ATTACKING SERVER
+start nuclear.bat
+timeout /t 0 >nul
+echo ATTACKING SERVER
+timeout /t 0 >nul
+color 2
+msg * hax!
+color 4
+echo ATTACKING SERVER
+timeout /t 0 >nul
+start nuclear.bat
+echo ATTACKING SERVER
+timeout /t 0 >nul
+echo ATTACKING SERVER
+start nuclear.bat
+eja
+msg * DIE!!
+color 2
+43459w356
+start nuclear.bat
+timeout /t 0 >nul
+4e5eisi5esa
+color 2
+msg * hacKED
+5iw385
+timeout /t 0 >nul
+start nuclear.bat
+se5iseij5se
+53w85uw
+color 40
+color 20
+timeout /t 0 >nul
+start nuclear.bat
+msg * DIE!!
+5esu5sieu5
+timeout /t 0 >nul
+color 2
+start nuclear.bat
+AAAAAAA
+e
+timeout /t 0 >nul
+peEEEEEEE
+color 4
+timeout /t 0 >nul
+RLLA
+start nuclear.bat
+msg * XD
+aeaeA
+color 40
+timeout /t 0 >nul
+ekakkk
+color 2
+start nuclear.bat
+timeout /t 0 >nul
+eoaoi49924
+394i923493
+color 20
+timeout /t 0 >nul
+color 40
+timeout /t 0 >nul
+start nuclear.bat
+249324uu3ui4pas
+msg * hax
+color 4
+start nuclear.bat
+timeout /t 0 >nul
+4sei483w
