e1eee3ccc388f0b075b6ce98ca2a1b3e36cfa1ba
FOX GAMINGYT <105371123+FoxGamingYTCR@users.noreply.github.com>
2022-05-11 11:42:36 -0400

    start of an amazing rpg game
    use it for your own code if you wish but please give me credit for it. thanks!

---
+++ RPG GAME INTRO.bat
@@ -0,0 +1,57 @@
+@echo off
+title start of an amazing rpg game!
+:1
+echo loading.
+ping localhost >nul
+cls
+echo loading..
+ping localhost >nul
+cls
+echo loading...
+ping localhost >nul
+cls
+echo loading.
+ping localhost >nul
+cls
+echo loading..
+ping localhost >nul
+cls
+echo loading...
+ping localhost >nul
+cls
+echo loading.
+ping localhost >nul
+cls
+echo loading..
+ping localhost >nul
+cls
+echo loading...
+ping localhost >nul
+cls
+echo loaded!
+pause
+cls
+echo ...where am i?
+pause
+cls
+echo .
+ping localhost /n 1 >nul
+cls
+echo ..
+ping localhost /n 1 >nul
+cls
+echo ...
+ping localhost /n 1 >nul
+cls
+echo welcome... uh... whats your name???
+set /p name=my name is...=
+echo yes! welcome %name% to the town of townname this is an example of a rpg game intro.
+pause
+cls
+echo if you want, you can use it for your own code. spread awarness of me(Fox GamingYT) and help me become more popular. ^w^
+pause
+cls
+echo anyways i'll let you go now. bye %name%!
+pause
+cls
+exit
